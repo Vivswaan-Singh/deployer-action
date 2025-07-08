b9fa63161dc1e3cc670c0cce9e9938e0ba73b4bc
aAnurag8 <anurag.chaturvedi@syvora.com>
2025-07-08 14:15:57 +0530

    enable ten BP fee amount

--- src/steps/add-1bp-fee-tier.ts
+++ src/steps/add-1bp-fee-tier.ts
@@ -5,6 +5,8 @@ import { sign } from 'crypto'
 
 const ONE_BP_FEE = 100
 const ONE_BP_TICK_SPACING = 1
+const TEN_BP_FEE = 1000
+const TEN_BP_TICK_SPACING = 20
 
 export const ADD_1BP_FEE_TIER: MigrationStep = async (state, { signer, gasPrice }) => {
   if (state.coreFactoryAddress === undefined) {
@@ -24,12 +26,19 @@ export const ADD_1BP_FEE_TIER: MigrationStep = async (state, { signer, gasPrice 
   if (owner != zeroAddr && owner !== (await signer.getAddress())) {
     throw new Error('Factory.owner is not signer')
   }
-  const tx = await coreFactory.enableFeeAmount(ONE_BP_FEE, ONE_BP_TICK_SPACING)
+  try{
+    const tx = await coreFactory.enableFeeAmount(ONE_BP_FEE, ONE_BP_TICK_SPACING)
+    const tx2 = await coreFactory.enableFeeAmount(TEN_BP_FEE, TEN_BP_TICK_SPACING)
+  } catch(err) {
+    console.log("error while executing enableFeeAmount",err)
+    return[{
+      message:`error while executing enableFeeAmount ${err}` 
+    }]
+  }
 
   return [
     {
       message: `Factory added a new fee tier ${ONE_BP_FEE / 100} bps with tick spacing ${ONE_BP_TICK_SPACING}`,
-      hash: tx.hash,
     },
   ]
 }
